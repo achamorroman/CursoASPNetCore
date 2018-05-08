9b0f2b98c46485f44b4c31dee9986652bbc93821
curso ASP Net Core <achamorroman@outlook.com>
2018-05-08 11:37:43 +0200

    Estamentos de control de código

---
+++ Control/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Sentencias de control de código
+
+            Console.WriteLine("Pulsa una tecla...");
+            var a = Console.ReadKey();
+            var teclaPulsada = a.KeyChar;
+
+            // IF - Else          
+            if(teclaPulsada == '5')
+            {
+                Console.WriteLine("Has pulsado 5");
+            }
+            else if(teclaPulsada=='8')
+            {
+                Console.WriteLine("Has pulsado 8");
+            }
+            else
+            {
+                Console.WriteLine("Vete a saber...");
+            }
+
+            // Switch
+            switch(teclaPulsada)
+            {
+                case 'a':
+                    Console.WriteLine("Añadir elemento");
+                    break;
+                case 'e':
+                    Console.WriteLine("Eliminar elemento");
+                    break;
+                case 'l':
+                    Console.WriteLine("Listar elementos");
+                    break;
+                default:
+                    break;
+            }
+
+            // Iteraciones
+
+            // While y bucle do-While
+            Console.WriteLine("Ejecutando While");
+            int i = 0;
+            while(i < 5)
+            {
+                Console.WriteLine(i++);
+            }
+
+            // do - while
+            Console.WriteLine("Ejecutando do-while");
+            int x = 0;
+            do
+            {
+                Console.WriteLine(x++);
+            }
+            while(x < 7);
+
+            // for
+            Console.WriteLine("Ejecutando for");
+            for(int b=0; b < 10; b++)
+            {
+                Console.WriteLine(b);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
