168a50c83b1fea9d9b2f6d07ddb1ad4918a34133
curso ASP Net Core <achamorroman@outlook.com>
2018-05-09 14:32:10 +0200

    MiAPI Initial Commit

---
+++ MiAPI/MiAPI/Services/FakeTodoServices.cs
@@ -0,0 +1,36 @@
+﻿using MiAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiAPI.Services
+{
+    public class FakeTodoServices
+    {
+        public FakeTodoServices() { }
+
+        public IList<TodoList> GetTodoLists()
+        {
+            var list1 = new TodoList()
+            {
+                Id = 1,
+                Name = "Lista de la compra",
+                Owner = "Antonio",
+                Items = new List<TodoItem>()
+                {
+                     new TodoItem(){ TodoListId=1, Description="Huevos" },
+                     new TodoItem(){ TodoListId=1, Description="Pan" },
+                     new TodoItem(){ TodoListId=1, Description="Mucha cerveza" },
+                     new TodoItem(){ TodoListId=1, Description="Magdalenas" },
+                }
+            };
+
+            var listCollection = new List<TodoList>
+            {
+                list1
+            };
+            return listCollection;
+        }
+    }
+}
