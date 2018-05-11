8080ba7fd380de423c9c36737d7bba8d18071cc7
curso ASP Net Core <achamorroman@outlook.com>
2018-05-11 11:21:20 +0200

    Middleware simple app.Use()

--- MiAPI/MiAPI/Startup.cs
+++ MiAPI/MiAPI/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using MiAPI.Persistence;
@@ -31,19 +32,37 @@ namespace MiAPI
                     options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
                 );
 
+            // Inyección de servicios necesarios para la aplicación
             services.AddTransient<ITodoService, TodoService>();
 
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) 
         {
+            loggerFactory
+                .AddEventSourceLogger()
+                .AddConsole();
+
+            var logger = loggerFactory.CreateLogger("Profiler");
+
             // Configuración del Pipeline de ASP NET Core
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.Use(async (context, next) =>
+            {
+                var watch = Stopwatch.StartNew();
+                await next();
+                var path = context.Request.Path;
+                var statusCode = context.Response.StatusCode;
+                var logString = $"Path= '{path}', status={statusCode}, time={watch.Elapsed}";
+
+                logger.LogInformation(logString);
+            });
 
             app.UseMvc();
         }
